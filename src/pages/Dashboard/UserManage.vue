370470803048f71740e6e5a58d4df98b6301fdf4
can67yang <102890349+can67yang@users.noreply.github.com>
2026-08-23 16:48:01 +0800

    修复 UserManage 组件中的表格数据绑定，将 :data 属性更改为 :rows

--- src/pages/Dashboard/UserManage.vue
+++ src/pages/Dashboard/UserManage.vue
@@ -64,7 +64,7 @@
     <q-card class="q-ma-md q-pa-sm">
       <q-table
         title="所有用户"
-        :data="users"
+        :rows="users"
         :columns="columns"
         row-key="name"
         :selected-rows-label="getSelectedString"
